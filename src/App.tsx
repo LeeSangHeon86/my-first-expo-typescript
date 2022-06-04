952f82a783c1010d83e90b1d03bff5a588117e6f
이상헌 <bleach8625@gmail.com>
2022-06-04 22:04:40 +0900

    20220604 22:04

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 import MyButton from './MyButton';
 
 export default function App() {
+  const [addition, setAddition] = useState<number>(0);
+  const [multiple, setMultiple] = useState<number>(1);
+
+  const calculate = () => {
+    setAddition(addition + 2);
+    setMultiple(multiple * 2);
+  };
   return (
     <View style={styles.container}>
       <Text>이상헌 첫 리액트 네이티브!!!!!!!</Text>
@@ -15,6 +22,13 @@ export default function App() {
       </MyButton>
       <MyButton onPress={() => Alert.alert('3st click')}>confirm</MyButton>
       <MyButton />
+      <Text style={{ fontSize: 20 }}>더하기 : {addition}</Text>
+      <Text style={{ fontSize: 20 }}>곱하기 : {multiple}</Text>
+      <Button title="Click" onPress={() => calculate()} />
+      <TextInput
+        style={{ borderColor: 'black', borderWidth: 1 }}
+        onChangeText={event => console.log(event)}
+      />
     </View>
   );
 }
