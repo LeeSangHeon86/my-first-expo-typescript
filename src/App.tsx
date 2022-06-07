e31d671db2c092ffb0a19c2515a4f0b311690b2a
이상헌 <bleach8625@gmail.com>
2022-06-07 17:10:38 +0900

    20220607 17:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Box from './Box';
-import Shadow from './Shadow';
+// import Box from './Box';
+// import Shadow from './Shadow';
+import styled, { ThemeProvider } from 'styled-components/native';
+import Input from './Input';
+import { Switch } from 'react-native';
+import { useState } from 'react';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }: { theme: themeProps }) => theme.bgColor};
+  align-items: center;
+  justify-content: center;
+`;
+
+const lightTheme = {
+  inputColor: 'black',
+  inputBorder: 'black',
+  bgColor: '#fff',
+};
+
+const darkTheme = {
+  inputColor: 'white',
+  inputBorder: 'white',
+  bgColor: 'black',
+};
+
+type themeProps = {
+  inputColor?: string;
+  inputBorder?: string;
+  bgColor?: string;
+};
 
 export default function App() {
+  const [isLight, toggleTheme] = useState(true);
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Shadow />
-      {/* <Box style={{ backgroundColor: 'red', height: 100 }} />
+    <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
+      <Container>
+        <StatusBar style="auto" />
+        <Switch
+          value={isLight}
+          onValueChange={isLight => toggleTheme(isLight)}
+        />
+        <Input placeholder="typing message" />
+        <Input />
+        {/* <Shadow /> */}
+        {/* <Box style={{ backgroundColor: 'red', height: 100 }} />
       <Box style={{ backgroundColor: 'blue', flex: 1 }} />
       <Box style={{ backgroundColor: 'pink', height: 100 }} /> */}
-    </View>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
