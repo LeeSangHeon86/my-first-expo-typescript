4a1728fb55daafd9c627d150049273ef5ba2ae90
이상헌 <bleach8625@gmail.com>
2022-06-07 17:20:45 +0900

    20220607 17:20

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
 
-const StyledInput = styled.TextInput.attrs(({ placeholder, theme }) => ({
-  placeholder: placeholder || 'Enter Text...',
-  placeholderTextColor: theme.inputColor,
-}))`
+const StyledInput = styled.TextInput.attrs(
+  ({
+    placeholder,
+    theme,
+  }: {
+    placeholder: string;
+    theme: themeProps['theme'];
+  }) => ({
+    placeholder: placeholder || 'Enter Text...',
+    placeholderTextColor: theme.inputColor,
+  }),
+)`
   padding: 20px;
   font-size: 20px;
-  border: 1px solid ${({ theme }) => theme.inputBorder};
+  border: 1px solid ${({ theme }: themeProps) => theme.inputBorder};
 `;
 
 type propsType = {
   placeholder?: string;
   text?: string;
+};
+
+type themeProps = {
+  theme: {
+    inputColor?: string;
+    inputBorder?: string;
+    bgColor?: string;
+  };
 };
 
 export default function Input({ placeholder }: propsType) {
