bcbbf906b3688df4a6405d132c002566bd6ad3a5
이상헌 <bleach8625@gmail.com>
2022-06-22 22:09:25 +0900

    20220622 20:00

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const Container = styled.View`
 `;
 
 export default function App() {
+  //20220622
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor={theme.background} barStyle="dark-content" />
